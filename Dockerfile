a30aabec5b14c6a08ffb3e31abc68a1b96004c1e
minseok Kim <minorchy@naver.com>
2024-09-24 20:33:59 +0900

    image change(10)

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,9 @@ WORKDIR /home/user
 # 현재 디렉토리의 .jar 파일을 파드 안으로 복사
 COPY build/libs/chattest-0.0.1-SNAPSHOT.jar /home/user/tuxtalk.jar
 
+# /upload 디렉토리 생성
+RUN mkdir -p /home/user/upload/default
+
 ## 로컬 시스템의 파일을 업로드 디렉토리로 복사 (선택 사항)
 #COPY /upload/2024/default/관리자님.jpg /hoem/user/upload/관리자님.jpg
 #COPY /upload/2024/default/남석진우뉨.jpg /home/user/upload/남석진우님.jpg
--- src/main/java/com/chattest/config/DataInitializer.java
+++ src/main/java/com/chattest/config/DataInitializer.java
@@ -23,31 +23,31 @@ public class DataInitializer implements CommandLineRunner {
                 .email("admin@com.chattest")
                 .name("CCCR관리자")
                 .password(passwordEncoder.encode("1234"))
-                .profilePictureUrl("2024/09/20/eb3e6354-4523-4326-adc6-7c297392cb84_관리자님.jpg")
+                .profilePictureUrl("home/user/upload/default/관리자님.jpg")
                 .build();
         User user1 = User.builder()
                 .email("user1@com.chattest")
                 .name("김민석")
                 .password(passwordEncoder.encode("1234"))
-                .profilePictureUrl("2024/09/20/ba01307c-6d31-4e13-9938-0366a5eff353_카카오톡_프사.jpg")
+                .profilePictureUrl("home/user/upload/default/카카오톡_프사.jpg")
                 .build();
         User user2 = User.builder()
                 .email("user2@com.chattest")
                 .name("최민정")
                 .password(passwordEncoder.encode("1234"))
-                .profilePictureUrl("2024/09/20/3737676e-4893-4167-acf0-959c35d5b126_민정님.jpg")
+                .profilePictureUrl("home/user/upload/default/민정님.jpg")
                 .build();
         User user3 = User.builder()
                 .email("user3@com.chattest")
                 .name("윤남석")
                 .password(passwordEncoder.encode("1234"))
-                .profilePictureUrl("2024/09/20/a9f43c1c-14ac-4bcb-b954-25e751a0c339_남석진우뉨.jpg")
+                .profilePictureUrl("home/user/upload/default/남석진우뉨.jpg")
                 .build();
         User user4 = User.builder()
                 .email("user4@com.chattest")
                 .name("이진우")
                 .password(passwordEncoder.encode("1234"))
-                .profilePictureUrl("2024/09/20/1addad11-79e4-4ad9-80da-5dc509fb6497_남석진우뉨.jpg")
+                .profilePictureUrl("home/user/upload/default/남석진우뉨.jpg")
                 .build();
 
         user.addUserRole(UserRole.USER);
